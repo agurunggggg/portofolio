b442fd7240f9a3d3830b58cfb89ecf46635ec24d
Sunil Gurung <sunil@prezzee.com>
2026-04-11 08:30:28 +1000

    Fixed the basepath for user site deployment

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 import { resolve } from "path";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/portofolio" : "",
   images: {
     unoptimized: true,
   },
